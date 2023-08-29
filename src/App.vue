cf04cf31e33197e09d8e653d4f0c3547afeb4570
Matheus Farias <matheuspaula@unimedceara.com.br>
2023-08-29 11:21:13 -0300

    Ajuste dos componentes

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
 <v-layout>
     <v-main class="d-flex flex-row align-center">
+      <v-app-bar color="transparent" class="pa-2" collapse :elevation="0">
+          <v-btn
+            :variant="theme.global.current.value.dark?'tonal':'text'" 
+            size="x-large"
+            :icon="theme.global.current.value.dark?'mdi-white-balance-sunny':'mdi-weather-night'" 
+            color="third" 
+            @click="toggleTheme">
+          </v-btn>            
+      </v-app-bar> 
       <v-container> 
-        <v-toolbar density="compact" color="transparent">
-            <v-btn 
-              variant="outlined" 
-              :icon="theme.global.current.value.dark?'mdi-weather-sunny':'mdi-weather-night'" 
-              color="third" 
-              @click="toggleTheme">
-            </v-btn>
-        </v-toolbar>            
         <router-view v-slot="{ Component, route }">
           <transition :name="route.meta.transitionName||' fade'" mode="out-in" append>
             <component :is="Component" />
--- src/components/about/index.vue
+++ src/components/about/index.vue
@@ -3,25 +3,33 @@
         <v-col cols="12">
             <v-row class="mb-6 px-6 py-0">
                 <v-col cols="7">
-                    <p class="text-h2 font-weight-bold ">
-                        Olá!!
-                    </p>
-                    <p class="text-h3 font-weight-bold text-orange-accent-3">
-                        Sou Matheus de Farias
-                    </p>
-                    <p class="text-h5 font-weight-bold">
-                        Seja muito bem vindo(a) ao meu site...
-                    </p>                    
+                    <transition name="slide-left" appear>
+                        <p class="text-h2 font-weight-bold text-primary">
+                            Olá!!
+                        </p>
+                    </transition>
+                    <transition name="slide-right" appear>
+                        <p class="text-h3 font-weight-bold text-secondary">
+                            Sou Matheus de Farias
+                        </p>
+                    </transition>
+                    <transition name="nested" appear>
+                        <p class="text-h5 font-weight-bold text-primary">
+                            Seja muito bem vindo(a) ao meu site...
+                        </p>                           
+                    </transition>             
                 </v-col>
                 <v-col cols="5" align-self="center">
                     <v-row>
                         <v-col align="center">
-                            <v-avatar color="primary" size="180" style="border: 3px solid #FF6D00">
-                                <v-img
-                                alt="Avatar"
-                                src="@/assets/img/me.gif"
-                              ></v-img>
-                              </v-avatar>
+                            <transition name="fade">
+                                <v-avatar variant="elevated" color="primary" size="180" class="border-color-primary">
+                                    <v-img
+                                        alt="Avatar"
+                                        src="@/assets/img/me.gif">
+                                    </v-img>
+                                </v-avatar>
+                            </transition>
                         </v-col>
                     </v-row>
 
@@ -29,7 +37,7 @@
             </v-row>
             <v-row justify="center">
                 <v-col cols="auto">
-                    <v-btn color="primary" variant="flat" icon="mdi-arrow-right" size="x-large"></v-btn>
+                    <v-btn color="secondary" variant="flat" icon="mdi-arrow-right" size="x-large"></v-btn>
                 </v-col>
             </v-row> 
         </v-col>
